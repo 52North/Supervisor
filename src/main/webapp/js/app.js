676052d7debb8e7d8088cd9bf77b6ff34c72a7a6
Daniel Nüst <d.nuest@52north.org>
2014-02-06 10:36:36 +0100

    reformat

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -16,20 +16,19 @@
 var supervisorApp = angular.module('supervisorApp', [ 'ngRoute',
 		'supervisorControllers' ]);
 
-//Install database onload - config settings
+// Install database onload - config settings
 supervisorApp.run(function(globals, $rootScope) {
-   // Your code here....
+	// Your code here....
 });
 
-//Define global variables
+// Define global variables
 supervisorApp.value('globals', {
-        db : null,
-        databasename : "serviceapp",
-        table1 : "serviceorders",
-        hasdata : false,
-        image_max_width: 620,
-    }
-);
+	db : null,
+	databasename : "serviceapp",
+	table1 : "serviceorders",
+	hasdata : false,
+	image_max_width : 620,
+});
 
 supervisorApp.config([ '$routeProvider', function($routeProvider) {
 	$routeProvider.when('/', {
