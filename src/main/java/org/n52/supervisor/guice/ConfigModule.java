42fc410f4f6f53ddd8d95f23d9ca6b3e11aae268
EHJ-52n <e.h.juerrens@52north.org>
2014-02-06 17:36:52 +0100

    fixes 52North/Supervisor#35
    Implement properties loading from user.home;

--- src/main/java/org/n52/supervisor/guice/ConfigModule.java
+++ src/main/java/org/n52/supervisor/guice/ConfigModule.java
@@ -16,8 +16,13 @@
 
 package org.n52.supervisor.guice;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
+import java.io.OutputStream;
 import java.util.Properties;
 
 import org.n52.supervisor.SupervisorProperties;
@@ -31,34 +36,70 @@ public class ConfigModule extends AbstractModule {
 
     private static Logger log = LoggerFactory.getLogger(ConfigModule.class);
 
-    private static Properties loadProperties(String name) {
-        URL url = ConfigModule.class.getResource(name);
-        log.trace("Loading properties for {} from {}", name, url);
+	private static final String USER_HOME_PATH = System.getProperty("user.home") + File.separator + ".SuperVisor" + File.separator;
 
-        Properties properties = new Properties();
-        try (InputStream s = url.openStream();) {
-            properties.load(s);
-            log.trace("Loaded properties: {}", properties);
-        }
-        catch (Exception e) {
-            log.error("Could not load properties file.", e);
-        }
+	private static final String DEFAULT_PATH = "/";
 
-        return properties;
+    private static Properties loadProperties(final String name) {
+    	log.trace("Loading properties '{}'", name);
+
+        final Properties properties = new Properties();
+    	try {
+			InputStream is;
+			String filePath = USER_HOME_PATH + name;
+			final File userHomeFile = new File(filePath);
+			if (!userHomeFile.exists()) {
+				filePath = DEFAULT_PATH + name;
+				is = ConfigModule.class.getResourceAsStream(filePath);
+				log.info("Load default properties from '{}'.", filePath);
+			} else if (!userHomeFile.canRead()) {
+				log.warn("Could not load properties. No reading permissions for '{}'.", userHomeFile);
+				log.info("Load default properties from jar file.");
+				filePath = DEFAULT_PATH + name;
+				is = ConfigModule.class.getResourceAsStream(filePath);
+			} else {
+				log.info("Load properties from '{}'.", userHomeFile);
+				is = new FileInputStream(userHomeFile);
+			}
+			properties.load(is);
+		} catch (final FileNotFoundException e) {
+			final String msg = "SuperVisor properties not found.";
+			log.error(msg, e);
+			throw new RuntimeException(msg,e);
+		} catch (final IOException e) {
+			final String msg = "SuperVisor properties not readable.";
+			log.error(msg, e);
+			throw new RuntimeException(msg,e);
+		}
+    	// try storing properties in user.home
+    	final File folder = new File(USER_HOME_PATH);
+		if (!folder.exists() && !folder.mkdir()) {
+			log.warn("SuperVisor properties could not be saved. No writing permissions at '{}'.", folder);
+			return properties;
+		}
+		final File file = new File(USER_HOME_PATH + name);
+		log.info("Save properties at '{}'.", file.getAbsolutePath());
+		try { //save properties
+			final OutputStream os = new FileOutputStream(file);
+			properties.store(os, null);
+		} catch (final IOException e) {
+			log.error("SuperVisor properties could not be saved.", e);
+		}
+		return properties;
     }
 
     @Override
     protected void configure() {
-        Properties appProps = loadProperties("/app.properties");
+        final Properties appProps = loadProperties("app.properties");
         Names.bindProperties(binder(), appProps);
 
-        Properties supervisorProps = loadProperties("/supervisor.properties");
+        final Properties supervisorProps = loadProperties("supervisor.properties");
         Names.bindProperties(binder(), supervisorProps);
 
         // FIXME remove manual instantiation of property manager
-        SupervisorProperties sp = SupervisorProperties.getInstance(supervisorProps);
+        final SupervisorProperties sp = SupervisorProperties.getInstance(supervisorProps);
         log.warn("Instantiated property manager manually: {}", sp);
-        
+
         log.info("Configured {}", this);
     }
 }
